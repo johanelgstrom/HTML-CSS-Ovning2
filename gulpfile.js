01d2976498e6e6176f640393b0b8cbba74b186dd
Johan Elgström <johan.elgstrom@gmail.com>
2021-10-04 10:36:55 +0200

    Clean up

--- gulpfile.js
+++ gulpfile.js
@@ -1,10 +1,12 @@
 var gulp = require("gulp");
 const sass = require("gulp-sass")(require("node-sass"));
+var cleanCSS = require("gulp-clean-css");
 
 gulp.task ("compile", function() {
     return gulp
         .src("scss/main.scss")
         .pipe (sass())
+        .pipe (cleanCSS())
         .pipe (gulp.dest("css"))
 });
 gulp.task ("watch-scss", function() {
